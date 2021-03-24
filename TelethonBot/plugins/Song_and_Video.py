05443473870f974327a6c8b24b30c26f9df62030
ᏗᎮ๏ɭɭ๏ <72794063+PmsHere@users.noreply.github.com>
2021-03-24 09:16:46 +0530

    Update Song_and_Video.py

--- TelethonBot/plugins/Song_and_Video.py
+++ TelethonBot/plugins/Song_and_Video.py
@@ -13,8 +13,8 @@ from base64 import b64decode
 from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo, DocumentAttributeAudio
 from telethon import events
-from Elizabeth.events import register
-from Elizabeth.utils import progress
+from BotzHub.events import register
+from BotzHub.utils import progress
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from validators.url import url
