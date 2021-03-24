501cbbe6b49d34bf435db32b3b60ca85e8f5d40d
ᏗᎮ๏ɭɭ๏ <72794063+PmsHere@users.noreply.github.com>
2021-03-24 08:34:13 +0530

    Add files via upload

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -1,1 +1,34 @@
+# By < @xditya >
+# // @BotzHub //
+from .. import BotzHub
+from telethon import events, Button
 
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply("Hello!",
+                    buttons=[
+                        [Button.url("ButtonUrl", url="https://t.me/xditya")],
+                        [Button.inline("T&C",data="example")]
+                    ])
+
+# callback for t&c button
+@BotzHub.on(events.callbackquery.CallbackQuery(data="example"))
+async def ex(event):
+    to_show = """
+T&C Apply
+
+This is a sample paragraph.
+.
+.
+"""
+    await event.edit(to_show,
+                    buttons=[Button.inline("Back", data="backbutt")])
+
+# callback for back button 
+@BotzHub.on(events.callbackquery.CallbackQuery(data="backbutt"))
+async def bacckk(event):
+    await event.edit("Hello!",
+                    buttons=[
+                        [Button.url("ButtonUrl", url="https://t.me/xditya")],
+                        [Button.inline("T&C",data="example")]
+                    ])
